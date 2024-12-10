aeb3b765c3a6b4efc1d20e2c9fb43da29204b437
Willian Cavalcante <willian.exercito@gmail.com>
2024-12-10 13:06:21 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,46 @@
 <?php
-// Importar configurações globais
+// Incluindo configurações globais e dependências necessárias
 require_once '../config/db.php';
 require_once '../config/auth.php';
 require_once '../config/helpers.php';
-require_once '../config/constants.php'; // Inclua a constante BASE_URL
+require_once '../config/constants.php';
 
-// Capturar a URL solicitada e remover qualquer parâmetro GET
+// Capturando a URL solicitada
 $request = $_SERVER['REQUEST_URI'];
-$request = strtok($request, '?'); // Ignorar parâmetros GET
+$request = strtok($request, '?'); // Removendo parâmetros GET da URL, se existirem
 
-// Roteamento baseado na URL
+// Verificando as rotas
 switch ($request) {
-    case '/':
-    case '/dashboard':
+    case '/': // Rota para a página inicial
+    case '/dashboard': // Painel principal do sistema
         include '../views/dashboard.php';
         break;
 
-    case '/login':
+    case '/login': // Rota para o login
         include '../controllers/LoginController.php';
         break;
 
-    case '/logout':
+    case '/logout': // Rota para sair do sistema
         include '../controllers/LogoutController.php';
         break;
 
-    case '/faturas':
+    case '/faturas': // Rota para listar faturas
         include '../controllers/FaturaController.php';
         break;
 
-    case '/faturas/cadastrar':
+    case '/faturas/cadastrar': // Rota para o formulário de lançamento de faturas
         include '../views/faturas/cadastrar.php';
         break;
 
-    case '/transportadoras':
+    case '/transportadoras': // Rota para listar transportadoras
         include '../controllers/TransportadoraController.php';
         break;
 
-    case '/transportadoras/cadastrar':
+    case '/transportadoras/cadastrar': // Rota para o formulário de cadastro de transportadoras
         include '../views/transportadoras/cadastrar.php';
         break;
 
-    default:
+    default: // Caso nenhuma rota seja encontrada
         http_response_code(404);
         echo "<h1>404 - Página não encontrada</h1>";
         break;
